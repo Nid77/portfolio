bd56facb264c4a0e2e8832eec0f9aa1dca8d5b4c
Nid77 <rayan.tail@etu.univ-lyon1.fr>
2025-01-09 21:50:58 +0100

    revenir en haut de page apres avoir cliquer sur un router-link

--- src/router/index.ts
+++ src/router/index.ts
@@ -46,7 +46,10 @@ const router = createRouter({
       name: 'error',
       component: ErrorView
     }
-  ]
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    return { top: 0 };
+  }
 })
 
 export default router
